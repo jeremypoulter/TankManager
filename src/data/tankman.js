ce2823557dd3396b15fbb89819fda171774ea122
Jeremy Poulter <jeremy@bigjungle.net>
2018-08-27 23:24:43 +0100

    Initial addition of code to add sceenes

--- src/data/tankman.js
+++ src/data/tankman.js
@@ -1,178 +1,175 @@
-var baseHost = window.location.hostname;
-//var baseHost = 'esplug.local';
+//var baseHost = window.location.hostname;
+var baseHost = 'tankman.local';
 //var baseHost = 'test.com';
-var baseEndpoint = 'http://'+baseHost;
+var baseEndpoint = 'http://' + baseHost;
 
-function WiFiViewModel()
-{
-    var self = this;
+function WiFiViewModel() {
+  var self = this;
 
-    ko.mapping.fromJS({
-        'mac': '00:00:00:00:00:00',
-        'localIP': '0.0.0.0',
-        'subnetMask': '255.255.255.0',
-        'gatewayIP': '0.0.0.0',
-        'dnsIP': '0.0.0.0',
-        'status': 0,
-        'hostname': 'tankman',
-        'SSID': 'ssid',
-        'BSSID': '00:00:00:00:00:00',
-        'RSSI': 0
-    }, {}, self);
+  ko.mapping.fromJS({
+    'mac': '00:00:00:00:00:00',
+    'localIP': '0.0.0.0',
+    'subnetMask': '255.255.255.0',
+    'gatewayIP': '0.0.0.0',
+    'dnsIP': '0.0.0.0',
+    'status': 0,
+    'hostname': 'tankman',
+    'SSID': 'ssid',
+    'BSSID': '00:00:00:00:00:00',
+    'RSSI': 0
+  }, {}, self);
 
-    self.fetching = ko.observable(false);
-    self.scan = ko.observable(new WiFiScanViewModel());
+  self.fetching = ko.observable(false);
+  self.scan = ko.observable(new WiFiScanViewModel());
 
-    self.update = function () {
-        self.scan().update();
-        self.fetching(true);
-        $.get(baseEndpoint+'/wifi', function (data) {
-            ko.mapping.fromJS(data, self);
-        }, 'json').always(function () {
-            self.fetching(false);
-        });
-    };
-}
-
-function WiFiScanResultViewModel(data)
-{
-    var self = this;
-    ko.mapping.fromJS(data, {}, self);
-}
-
-function WiFiScanViewModel()
-{
-    var self = this;
-
-    self.results = ko.mapping.fromJS([], {
-        key: function(data) {
-            return ko.utils.unwrapObservable(data.ssid);
-        },
-        create: function (options) {
-            return new WiFiScanResultViewModel(options.data);
-        }
+  self.update = function () {
+    self.scan().update();
+    self.fetching(true);
+    $.get(baseEndpoint + '/wifi', function (data) {
+      ko.mapping.fromJS(data, self);
+    }, 'json').always(function () {
+      self.fetching(false);
     });
-
-    self.fetching = ko.observable(false);
-
-    self.update = function () {
-        self.fetching(true);
-        $.get(baseEndpoint+'/wifi/scan', function (data) {
-            ko.mapping.fromJS(data, self.results);
-        }, 'json').always(function () {
-            self.fetching(false);
-        });
-    };
+  };
 }
 
-function InfoViewModel()
-{
-    var self = this;
-
-    ko.mapping.fromJS({
-        'id': 0,
-        'heap': 0,
-        'version': '0.0.0'
-    }, {}, self);
-
-    self.fetching = ko.observable(false);
-
-    self.update = function () {
-        self.fetching(true);
-        $.get(baseEndpoint+'/info', function (data) {
-            ko.mapping.fromJS(data, self);
-        }, 'json').always(function () {
-            self.fetching(false);
-        });
-    };
+function WiFiScanResultViewModel(data) {
+  var self = this;
+  ko.mapping.fromJS(data, {}, self);
 }
 
-function ConfigViewModel()
-{
-    var self = this;
+function WiFiScanViewModel() {
+  var self = this;
 
-    ko.mapping.fromJS({
-        'wifiClientSsid': '',
-        'wifiClientPassword': '',
-        'wifiHostname': 'tankman',
-        'mqttHostname': '',
-        'mqttTopic': 'fishtank',
-        'mqttUsername': '',
-        'mqttPassword': ''
-    }, {}, self);
+  self.results = ko.mapping.fromJS([], {
+    key: function (data) {
+      return ko.utils.unwrapObservable(data.ssid);
+    },
+    create: function (options) {
+      return new WiFiScanResultViewModel(options.data);
+    }
+  });
 
-    self.fetching = ko.observable(false);
+  self.fetching = ko.observable(false);
 
-    self.update = function () {
-        self.fetching(true);
-        $.get(baseEndpoint+'/settings', function (data) {
-            ko.mapping.fromJS(data, self);
-        }, 'json').always(function () {
-            self.fetching(false);
-        });
-    };
-
-    self.save = function() {
-        self.fetching(true);
-        $.post(baseEndpoint+'/settings', JSON.stringify({
-            'wifiClientSsid': self.wifiClientSsid(),
-            'wifiClientPassword': self.wifiClientPassword(),
-            'wifiHostname': self.wifiHostname()
-        }), function (data) {
-        }, 'json').always(function () {
-            self.fetching(false);
-        });
-    };
+  self.update = function () {
+    self.fetching(true);
+    $.get(baseEndpoint + '/wifi/scan', function (data) {
+      ko.mapping.fromJS(data, self.results);
+    }, 'json').always(function () {
+      self.fetching(false);
+    });
+  };
 }
 
-function SettingsViewModel(app)
-{
+function InfoViewModel() {
+  var self = this;
+
+  ko.mapping.fromJS({
+    'id': 0,
+    'heap': 0,
+    'version': '0.0.0'
+  }, {}, self);
+
+  self.fetching = ko.observable(false);
+
+  self.update = function () {
+    self.fetching(true);
+    $.get(baseEndpoint + '/info', function (data) {
+      ko.mapping.fromJS(data, self);
+    }, 'json').always(function () {
+      self.fetching(false);
+    });
+  };
+}
+
+function ConfigViewModel() {
+  var self = this;
+
+  ko.mapping.fromJS({
+    'wifiClientSsid': '',
+    'wifiClientPassword': '',
+    'wifiHostname': 'tankman',
+    'mqttHostname': '',
+    'mqttTopic': 'fishtank',
+    'mqttUsername': '',
+    'mqttPassword': ''
+  }, {}, self);
+
+  self.fetching = ko.observable(false);
+
+  self.update = function () {
+    self.fetching(true);
+    $.get(baseEndpoint + '/settings', function (data) {
+      ko.mapping.fromJS(data, self);
+    }, 'json').always(function () {
+      self.fetching(false);
+    });
+  };
+
+  self.save = function () {
+    self.fetching(true);
+    $.post(baseEndpoint + '/settings', JSON.stringify({
+      'wifiClientSsid': self.wifiClientSsid(),
+      'wifiClientPassword': self.wifiClientPassword(),
+      'wifiHostname': self.wifiHostname()
+    }), function (data) {
+    }, 'json').always(function () {
+      self.fetching(false);
+    });
+  };
+}
+
+function SettingsViewModel(app) {
   var self = this;
 
   self.wifi = ko.observable(new WiFiViewModel());
   self.config = ko.observable(new ConfigViewModel());
 
   app.isSettings.subscribe(function (selected) {
-      if (selected) {
-          self.wifi().update();
-          self.config().update();
-      }
+    if (selected) {
+      self.wifi().update();
+      self.config().update();
+    }
   });
 
   self.setSsid = function (item) {
-      self.config().wifiClientSsid(item.ssid());
-      self.config().wifiClientPassword('');
+    self.config().wifiClientSsid(item.ssid());
+    self.config().wifiClientPassword('');
   };
 }
 
-function AboutViewModel(app)
-{
+function AboutViewModel(app) {
   var self = this;
 
   self.info = ko.observable(new InfoViewModel());
 
   self.reboot = function () {
-    $.post(baseEndpoint+'/reboot', function (data) {
+    $.post(baseEndpoint + '/reboot', function (data) {
       alert('ESPlug rebooting');
     }, 'json');
   };
 
   self.factoryReset = function () {
     $.ajax({
-        url: baseEndpoint+'/settings',
-        type: 'DELETE'
+      url: baseEndpoint + '/settings',
+      type: 'DELETE'
     });
   };
 
   app.isAbout.subscribe(function (selected) {
-      if (selected) {
-          self.info().update();
-      }
+    if (selected) {
+      self.info().update();
+    }
   });
 }
 
-function SceenViewModel(app)
-{
+function Scene(data) {
+  var self = this;
+  ko.mapping.fromJS(data, {}, self);
+}
+
+function SceneViewModel(tank) {
   var self = this;
 
   self.id = ko.observable(1);
@@ -184,76 +181,102 @@ function SceenViewModel(app)
   self.ch2 = ko.observable(0);
   self.ch3 = ko.observable(0);
 
+  self.maxId = ko.pureComputed(function () {
+    var id = 0;
+    var scenes = tank.scenes();
+    for (const scene of scenes) {
+      if(scene.id() > id) {
+        id = scene.id();
+      }
+    }
+
+    return id;
+  });
+
   self.save = function () {
-  }
+    tank.scenes.push(new Scene({
+      id: parseInt(self.id()),
+      name: self.name(),
+      fadeIn: parseInt(self.fadeIn()),
+      fadeOut: parseInt(self.fadeOut()),
+      ch1: self.ch1(),
+      ch2: self.ch2(),
+      ch3: self.ch3()
+    }));
+
+    self.id(self.maxId() + 1);
+    self.name('');
+  };
 }
 
-function EnviromentViewModel(app)
-{
+function EnviromentViewModel(app) {
   var self = this;
 
+  self.temp = ko.observable(28.4);
 }
 
-function TankViewModel(app)
-{
+function TankViewModel(app) {
   var self = this;
 
-  self.sceen = ko.observable(new SceenViewModel(app));
+  self.scene = ko.observable(new SceneViewModel(self));
+  self.scenes = ko.observableArray([]);
   self.enviroment = ko.observable(new EnviromentViewModel(app));
 
   app.isTank.subscribe(function (selected) {
-      if (selected) {
-      }
+    if (selected) {
+    }
   });
 }
 
-function TankManagerViewModel()
-{
-    var self = this;
+function TankManagerViewModel() {
+  var self = this;
 
-    // Data
-    self.tab = ko.observable(null);
+  // Data
+  self.tab = ko.observable(null);
 
-    // Derived data
-    self.isTank = ko.pureComputed(function () {
-        return this.tab() == 'tank';
-    }, this);
-    self.isSettings = ko.pureComputed(function () {
-        return this.tab() == 'settings';
-    }, this);
-    self.isAbout = ko.pureComputed(function () {
-        return this.tab() == 'about';
-    }, this);
+  // Derived data
+  self.isTank = ko.pureComputed(function () {
+    return this.tab() == 'tank';
+  }, this);
+  self.isSettings = ko.pureComputed(function () {
+    return this.tab() == 'settings';
+  }, this);
+  self.isAbout = ko.pureComputed(function () {
+    return this.tab() == 'about';
+  }, this);
 
-    // Behaviours
-    self.goToTab = function (tab) { location.hash = tab; };
+  // Behaviours
+  self.goToTab = function (tab) { location.hash = tab; };
 
-    // Tank
-    self.tank = ko.observable(new TankViewModel(self));
+  self.showSceen = function(id) {
+    
+  }
 
-    // Settings
-    self.settings = ko.observable(new SettingsViewModel(self));
+  // Tank
+  self.tank = ko.observable(new TankViewModel(self));
 
-    // About
-    self.about = ko.observable(new AboutViewModel(self));
+  // Settings
+  self.settings = ko.observable(new SettingsViewModel(self));
 
-    // Client-side routes
-    var sammy = Sammy(function ()
-    {
-        this.get('#:tab', function () {
-            self.tab(this.params.tab);
-        });
+  // About
+  self.about = ko.observable(new AboutViewModel(self));
 
-        this.get('', function () {
-            this.redirect('#settings');
-        });
+  // Client-side routes
+  var sammy = Sammy(function () {
+    this.get('#:tab', function () {
+      self.tab(this.params.tab);
     });
 
-    sammy.run();
+    this.get('', function () {
+      this.redirect('#settings');
+    });
+  });
+
+  sammy.run();
 }
 
-$(".fader").slider({
-	reversed : true
+$('.fader').slider({
+  reversed: true
 });
 
 
